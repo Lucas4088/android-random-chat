747496d13409bf0af7ce9d230e9eb9d3f3da8ba6
Lucas <lukasz4088@gmail.com>
2017-05-20 00:56:54 +0200

    adding activity to manifest

--- app/src/main/java/com/example/androidrandomchat/DisplayMessageActivity.java
+++ app/src/main/java/com/example/androidrandomchat/DisplayMessageActivity.java
@@ -1,8 +1,10 @@
 package com.example.androidrandomchat;
 
-import android.content.Intent;
-import android.os.Bundle;
+
+
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
 
 
 public class DisplayMessageActivity extends AppCompatActivity {
--- app/src/main/java/com/example/androidrandomchat/MainActivity.java
+++ app/src/main/java/com/example/androidrandomchat/MainActivity.java
@@ -16,8 +16,6 @@ public class MainActivity extends AppCompatActivity {
     public void startChat(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         startActivity(intent);
-
-
     }
 
 }
