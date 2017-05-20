a06c4f047b90cea515cf16e52c3a3145907f13eb
Lucas <lukasz4088@gmail.com>
2017-05-20 18:59:10 +0200

    spelling mistake

--- app/src/main/java/com/example/androidrandomchat/MainActivity.java
+++ app/src/main/java/com/example/androidrandomchat/MainActivity.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity1 extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +14,7 @@ public class MainActivity1 extends AppCompatActivity {
     }
 
     public void startChat(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity1.class);
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
         startActivity(intent);
     }
 
